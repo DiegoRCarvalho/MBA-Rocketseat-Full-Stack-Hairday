fa11f5c10c22c31b27bd62428c23a7f571cd6ef2
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-10-25 14:25:27 -0300

    send selected time

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectDate = document.getElementById("date")
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 
@@ -11,6 +12,37 @@ selectDate.min = inputToday
 form.onsubmit = (event) => {
   // prevents default form submission behavior
   event.preventDefault()
+  try {
 
-  console.log("Enviado")
+    // Retrieve customer name.
+    const name = clientName.value.trim()
+    if(!name){
+      return alert("Informe o nome do cliente.")
+    }
+
+    // Retrieve the selected hour.
+    const hourSelected = document.querySelector(".hour-selected")
+    if(!hourSelected){
+      return alert("Informe a hora.")
+    }
+    
+    // Recover only the hour.
+    const [hour] = hourSelected.innerText.split(":")
+
+    // Insert time into date.
+    const when = dayjs(selectDate.value).add(hour,"hour")
+
+    // Generate an ID
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name, 
+      when
+    })
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.")
+    console.log(error)
+  }
 }
